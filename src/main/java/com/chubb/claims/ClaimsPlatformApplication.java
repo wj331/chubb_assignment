2f3c045af03cea30bb220d747672dd9054efc7a5
wj331 <e0389091@u.nus.edu>
2026-07-30 16:06:05 +0800

    first commit

---
+++ src/main/java/com/chubb/claims/ClaimsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.chubb.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsPlatformApplication.class, args);
+	}
+
+}
